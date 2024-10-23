724d1f475b7c9cf0205658c6c3051f6cdf25ecd9
AmbikaPaaru <ambikanagappa6@gmail.com>
2024-10-23 22:57:23 +0530

    add function to calulate the given string using delimiter

--- calculator.js
+++ calculator.js
@@ -3,10 +3,23 @@ function add(value) {
     return 0;
   }
   let delimiter = /,|\n/;
-  if (!value.includes(",") || !value.includes("/n")) {
-    return value.split("").join(",");
+  if (value.startsWith("//")) {
+    const splittedValue = value.split("\n", 2);
+    if (splittedValue?.length < 2) {
+      throw new Error("Delimiter and numbers must be provided.");
+    }
+    delimiter = new RegExp(splittedValue[0].substring(2));
+    value = splittedValue[1];
   }
-  const numbersArray = value.split(delimiter).map(Number);
+
+  const numbersArray = value
+    .split(delimiter)
+    .map((num) => {
+      const trimmedNum = num.trim();
+      return trimmedNum === "" ? null : Number(trimmedNum);
+    })
+    .filter((num) => num !== null && !isNaN(num));
+
   const negatives = numbersArray.filter((num) => num < 0);
   if (negatives.length > 0) {
     throw new Error(`Negative numbers not allowed: ${negatives.join(", ")}`);
@@ -15,11 +28,10 @@ function add(value) {
 }
 
 function calculator() {
-  const inputCount = document.getElementById("numbersInput").value;
+  const inputCount = document.getElementById("numbersInput").value.trim();
   try {
     const finalResult = add(inputCount);
     document.getElementById("result").textContent = finalResult;
-    console.log(finalResult);
   } catch (err) {
     document.getElementById("result").textContent = err.message;
   }
