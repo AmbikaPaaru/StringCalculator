80d32178d550bf2235d917f0279d62c9a292c8a5
AmbikaPaaru <ambikanagappa6@gmail.com>
2024-10-23 21:28:39 +0530

    written a function to calculate if we get a value

--- calculator.js
+++ calculator.js
@@ -1,3 +1,13 @@
+function add(value) {
+  return value;
+}
+
 function calculator() {
-    return 0
+  const inputCount = document.getElementById("numbersInput").value;
+  try {
+    const finalResult = add(inputCount);
+    document.getElementById("result").textContent = finalResult;
+  } catch (err) {
+    document.getElementById("result").textContent = err.message;
+  }
 }
