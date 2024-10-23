a8532cb27141de25e7f0744b0a9c8f50f3f0c520
AmbikaPaaru <ambikanagappa6@gmail.com>
2024-10-23 22:04:20 +0530

    add 2 numbers and thow message if its negative number

--- calculator.js
+++ calculator.js
@@ -1,5 +1,17 @@
 function add(value) {
-  return value;
+  if (value === "") {
+    return 0;
+  }
+  let delimiter = /,|\n/;
+  if (!value.includes(",") || !value.includes("/n")) {
+    return value.split("").join(",");
+  }
+  const numbersArray = value.split(delimiter).map(Number);
+  const negatives = numbersArray.filter((num) => num < 0);
+  if (negatives.length > 0) {
+    throw new Error(`Negative numbers not allowed: ${negatives.join(", ")}`);
+  }
+  return numbersArray.reduce((sum, num) => sum + num, 0);
 }
 
 function calculator() {
@@ -7,6 +19,7 @@ function calculator() {
   try {
     const finalResult = add(inputCount);
     document.getElementById("result").textContent = finalResult;
+    console.log(finalResult);
   } catch (err) {
     document.getElementById("result").textContent = err.message;
   }
